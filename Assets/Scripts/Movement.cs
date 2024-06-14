152baeb19dd360d84d58642170b31398a19b4a74
Samuel Abiodun <81042468+SamAbiodun@users.noreply.github.com>
2024-06-14 22:09:32 +0100

    Level Design, Player Movement and Aim
    Completed level design, Player movement controls, and Aim.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class Movement: MonoBehaviour
 {
-    [SerializeField] float speed = 1; 
-    
+    [SerializeField] float speed = 5;
+
+    Vector3 turn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Receive inputs
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        Move();
+        Aim();
+    }
 
-        //Mouse movement
-        //float mLeft = Input.GetAxis("GetMouse")
+    private void Move()
+    {
+        //Receive inputs
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
         //Move Player
         Vector3 temp = new Vector3(horizontalInput, verticalInput, 0);
         temp = temp.normalized * speed * Time.deltaTime;
-
         transform.position += temp;
+    }
+
+    void Aim()
+    {
+        //Mouse movement
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        float zRot = Mathf.Atan2(turn.y, turn.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(0, 0, zRot);
     }
 }
 
