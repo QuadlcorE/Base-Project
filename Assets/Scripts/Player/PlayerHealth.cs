7d5fe6737b89adedb0862b0d569cb366ed3e69ef
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-06-25 20:03:25 +0100

    add: juggernaut functiionality

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -3,21 +3,81 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public const int MaxHealth = 100;
-    private int _health;
+    private int _currentHealth;
+    private bool _isRegenerating;
+    private float _regenRate = 5f; // Health per second
+    private bool _hasJuggernaut;
 
     private void Start()
     {
-        _health = MaxHealth;
+        _currentHealth = MaxHealth;
+        Debug.Log($"Initial Health: {_currentHealth}");
     }
 
-    public void TakeDamage(int damagePoints)
+    private void Update()
     {
-        _health -= damagePoints;
-        Debug.Log(_health);
+        if (_isRegenerating)
+        {
+            RegenerateHealth();
+        }
     }
 
-    public int GetHealth()
+    private void RegenerateHealth()
     {
-        return _health;
+        if (_currentHealth < MaxHealth)
+        {
+            _currentHealth += (int)(_regenRate * Time.deltaTime);
+            _currentHealth = Mathf.Min(_currentHealth, MaxHealth);
+            Debug.Log($"Health regenerating: {_currentHealth}");
+        }
+    }
+
+    public void EnableRegeneration()
+    {
+        _isRegenerating = true;
+        Debug.Log("Health regeneration enabled");
+    }
+
+    public void DisableRegeneration()
+    {
+        _isRegenerating = false;
+        Debug.Log("Health regeneration disabled");
+    }
+
+    public void EnableJuggernaut()
+    {
+        _hasJuggernaut = true;
+        Debug.Log("Juggernaut mode enabled");
+    }
+
+    public void DisableJuggernaut()
+    {
+        _hasJuggernaut = false;
+        Debug.Log("Juggernaut mode disabled");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (_hasJuggernaut)
+        {
+            damage /= 2; // Reduce damage by half
+            Debug.Log("Juggernaut mode active: Damage reduced");
+        }
+
+        _currentHealth -= damage;
+        Debug.Log($"Player took damage: {_currentHealth}");
+
+        if (_currentHealth <= 0)
+        {
+            // Handle player death
+            Debug.Log("Player died");
+        }
+    }
+
+    public void DisableAllPowerUps()
+    {
+        DisableRegeneration();
+        DisableJuggernaut();
+        Debug.Log("All power-ups disabled");
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,59 +1,16 @@
-using Fusion;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    /// <summary>
-    /// 2D Vector containing the mouse x and y axes
-    /// </summary>
-    public Vector3 turn { get; private set; }
-
-    /// <summary>
-    /// The point from which all bullets and projectiles spawn.
-    /// </summary>
     [SerializeField] private Transform _spawnPoint;
-
-    /// <summary>
-    /// Default Bullet prefab.
-    /// </summary>
     [SerializeField] private Bullet _bullet;
-
-    /// <summary>
-    /// Turret prefab.
-    /// </summary>
     [SerializeField] private Turret _turret;
-
-    /// <summary>
-    /// Canon prefab.
-    /// </summary>
     [SerializeField] private Canon _canon;
-
-    /// <summary>
-    /// Dropbox prefab.
-    /// </summary>
     [SerializeField] private DropBox _dropBox;
 
-    /// <summary>
-    /// 
-    /// </summary>
     private PlayerMovement _playerMovement;
-    
-    /// <summary>
-    /// 
-    /// </summary>
     private PlayerAiming _playerAiming;
-    
-    /// <summary>
-    /// 
-    /// </summary>
     private PlayerHealth _playerHealth;
-    
-    /// <summary>
-    /// 
-    /// </summary>
     private PowerUpManager _powerUpManager;
 
     private void Awake()
@@ -78,11 +35,11 @@ public class PlayerController : MonoBehaviour
         {
             FireBullet();
         }
-        
+
         // Use the active power-up from the PowerUpManager
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            FireActivePowerUp();
+            UseActivePowerUp();
         }
     }
 
@@ -91,10 +48,12 @@ public class PlayerController : MonoBehaviour
         Instantiate(_bullet, _spawnPoint.position, Quaternion.identity);
     }
 
-
-    private void FireActivePowerUp()
+    private void UseActivePowerUp()
     {
+        _playerHealth.DisableAllPowerUps();
+
         string currentPowerUp = _powerUpManager.GetCurrentPowerUp();
+        Debug.Log($"Activating power-up: {currentPowerUp}");
 
         switch (currentPowerUp)
         {
@@ -110,7 +69,13 @@ public class PlayerController : MonoBehaviour
                 Instantiate(_dropBox, _spawnPoint.position, transform.rotation);
                 break;
 
-            // Add cases for other power-ups
+            case "Regen":
+                _playerHealth.EnableRegeneration();
+                break;
+
+            case "Juggernaut":
+                _playerHealth.EnableJuggernaut();
+                break;
 
             default:
                 Debug.LogWarning("No valid power-up selected");
